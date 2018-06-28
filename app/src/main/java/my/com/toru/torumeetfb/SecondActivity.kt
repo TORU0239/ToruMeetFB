9d3b26be169a2b6b80d70f35e70c209c40e2bcc6
Toru Choi <toru.choi@11street.my>
2018-06-28 13:41:05 +0800

    qr code generator

--- app/src/main/java/my/com/toru/torumeetfb/SecondActivity.kt
+++ app/src/main/java/my/com/toru/torumeetfb/SecondActivity.kt
@@ -2,6 +2,8 @@ package my.com.toru.torumeetfb
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions
 
 class SecondActivity : AppCompatActivity() {
 
@@ -9,5 +11,16 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
         supportActionBar?.title = "SeondActivity"
+
+        init()
+    }
+
+    fun init(){
+        val options = FirebaseVisionBarcodeDetectorOptions
+                        .Builder()
+                        .setBarcodeFormats(FirebaseVisionBarcode.FORMAT_QR_CODE)
+                        .build()
+
+
     }
 }
